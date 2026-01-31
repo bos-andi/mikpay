cf42e29d044320d9b9970d1f4a49d986df7f49d2
Muhammad Andi <andi@mikpay.id>
2026-01-31 13:52:23 +0700

    Fix redirect path in settings/sessions.php

--- settings/sessions.php
+++ settings/sessions.php
@@ -23,7 +23,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 if (!isset($_SESSION["mikpay"])) {
-  header("Location:../admin.php?id=login");
+  header("Location:./admin.php?id=login");
   exit;
 } else {
   
